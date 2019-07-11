3680fb82b6ce9cede1b7f4f0c9cb6cb594c7232e
hieudeptrai09 <34306914+hieudeptrai09@users.noreply.github.com>
2019-07-12 00:02:46 +0700

    Update ThreadPool.java

--- ThreadPool/src/threadpool/ThreadPool.java
+++ ThreadPool/src/threadpool/ThreadPool.java
@@ -2,12 +2,12 @@ package threadpool;
 
 import java.util.LinkedList;
 
-public class ThreadPool {
+public class ThreadPool implements Pool {
 
     private LinkedList<Runnable> pool;
     private int capacity;
     private final int DEFAULT_CAPACITY = 10;
-    private final int MAX_CAPACITY = 3000;
+    private final int MAX_CAPACITY = 30;
     private int waitingAddCount = 0;
     private int waitingRemoveCount = 0;
 
@@ -25,11 +25,13 @@ public class ThreadPool {
         this.capacity = DEFAULT_CAPACITY;
     }
 
+    @Override
     public synchronized void execute() {
         Runnable task = pool.remove();
         task.run();
     }
 
+    @Override
     public synchronized void add(Runnable task) throws InterruptedException {
         if (pool.size() < capacity) {
             pool.add(task);
@@ -41,19 +43,40 @@ public class ThreadPool {
         }
     }
 
+    @Override
     public synchronized Runnable delete() {
         return pool.remove();
     }
 
+    @Override
     public void shutdown() {
-        
     }
 
+    @Override
     public int size() {
         return pool.size();
     }
 
-    public void resize(int newSize) {
-        capacity = newSize;
+    @Override
+    public synchronized void resize(int newSize) {
+        if (newSize > pool.size()) {
+            if (newSize > MAX_CAPACITY) {
+                throw new IllegalArgumentException("The size is greater than the max capacity");
+            }
+            capacity = newSize;
+        } else {
+            throw new IllegalArgumentException("This size you want to resize is smaller than the size of queue now");
+        }
     }
+
+    @Override
+    public int getMaxCapacity() {
+        return MAX_CAPACITY;
+    }
+
+    @Override
+    public int getCapacity() {
+        return capacity;
+    }
+
 }
